aa66c1d4651db5589f9b6779ab7595899a96c69f
alxndrpsclt <alxndr.psclt@gmail.com>
2021-01-10 22:40:00 +0100

    Adds basics of lightdm and bspwm config (including wallpapers)

---
+++ dev-install-last.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+#workon ansible
+#ansible-playbook --ask-become-password --ask-vault-password playbook.yml
+ansible-playbook -i inventory.yml --tags "last" --vault-password-file vault-password-file --extra-vars '@vars/passwords/passwords.yml' playbook.yml
