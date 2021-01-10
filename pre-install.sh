cbe0660e1c8582a71e12d690aa9cc4e75b2a500a
alxndrpsclt <alxndr.psclt@gmail.com>
2021-01-10 21:49:32 +0100

    Works on autosetup of mnml os

--- pre-install.sh
+++ pre-install.sh
@@ -1,1 +1,8 @@
 # Install postmarket os with no interface on the device, make sure ypu can connect by ssh.
+
+#connect to the internet; probably via wifi
+# sudo nmtui
+
+sudo apk update
+sudo apk upgrade
+sudo apk add python3
